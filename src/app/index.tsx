2842f682b12d3fda99348b1f0e184f32c7caa173
Alyona-K <alyonakruychkova@gmail.com>
2025-11-02 00:05:28 +0300

    Chore: start SCSS preprocessor setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Providers } from "./Providers";
 import { AppInit } from "./AppInit";
-import "./normalize.css";
-import "./index.css";
+import "@/shared/styles/styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
