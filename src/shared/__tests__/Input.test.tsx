4995f736dd4343ef20ea9e47d6bfb9b0aeed2c4e
Alyona-K <alyonakruychkova@gmail.com>
2025-11-17 22:30:56 +0300

    Fix: Input.test.tsx

--- src/shared/__tests__/Input.test.tsx
+++ src/shared/__tests__/Input.test.tsx
@@ -1,5 +1,8 @@
 // --- MOCK STATIC ASSETS ---
-jest.mock("@/assets/images/sprite.svg", () => "sprite-mock");
+jest.mock("@/assets/images/sprite.svg", () => ({
+  __esModule: true,
+  default: "sprite-mock",
+}));
 
 import { render, screen, fireEvent } from "@testing-library/react";
 import Input from "@shared/ui/Input";
